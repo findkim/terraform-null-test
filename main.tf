99255ddbf126fc5da763ca3306f985cb20678198
findkim <kngo@hashicorp.com>
2020-01-21 16:27:53 -0600

    provider source test

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
 resource "null_resource" "test" {}
+
+provider "template" {}
+
+terraform {
+  required_providers {
+    null = {
+      version = ">= 2.0.0"
+    }
+    gooddocs = {
+      source = "findkim/gooddocs"
+      version = "0.1.4"
+    }
+  }
+}
